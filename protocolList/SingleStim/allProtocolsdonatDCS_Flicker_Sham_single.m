1a1c3ec429852da51a3ac35847b337e632b3e649
Niloy Maity <niloymaity@iisc.ac.in>
2025-04-22 19:55:08 +0530

    minor changes

---
+++ protocolList/SingleStim/allProtocolsdonatDCS_Flicker_Sham_single.m
@@ -0,0 +1,15 @@
+function [expDates,protocolNames,stimType] = allProtocolsdonatDCS_Flicker_Sham_single
+
+clear index; index=1;expDates{index} = '300823'; protocolNames{index} = 'GRF_007'; stimType{index} = 4; % Pre-Sham, SFOriCon,4x4x3
+clear index; index=2;expDates{index} = '300823'; protocolNames{index} = 'GRF_008'; stimType{index} = 4; % Sham, Blank screen, SFOriCon,1x1x1
+clear index; index=3;expDates{index} = '300823'; protocolNames{index} = 'GRF_009'; stimType{index} = 4; % Post-Sham, SFOriCon,4x4x3
+clear index; index=4;expDates{index} = '300823'; protocolNames{index} = 'GRF_010'; stimType{index} = 4; % Post-Sham, SFOriCon,4x4x3
+clear index; index=5;expDates{index} = '300823'; protocolNames{index} = 'GRF_011'; stimType{index} = 4; % Post-Sham, SFOriCon,4x4x3
+clear index; index=6;expDates{index} = '300823'; protocolNames{index} = 'GRF_012'; stimType{index} = 4; % Post-Sham, SFOriCon,4x4x3
+
+clear index; index=7;expDates{index} = '080923'; protocolNames{index} = 'GRF_001'; stimType{index} = 4; % Pre-Sham, SFOriCon,4x4x3
+clear index; index=8;expDates{index} = '080923'; protocolNames{index} = 'GRF_002'; stimType{index} = 4; % Sham, Blank screen, SFOriCon,1x1x1
+clear index; index=9;expDates{index} = '080923'; protocolNames{index} = 'GRF_003'; stimType{index} = 4; % Post-Sham, SFOriCon,4x4x3
+clear index; index=10;expDates{index} = '080923'; protocolNames{index} = 'GRF_004'; stimType{index} = 4; % Post-Sham, SFOriCon,4x4x3
+clear index; index=11;expDates{index} = '080923'; protocolNames{index} = 'GRF_005'; stimType{index} = 4; % Post-Sham, SFOriCon,4x4x3
+clear index; index=12;expDates{index} = '080923'; protocolNames{index} = 'GRF_006'; stimType{index} = 4; % Post-Sham, SFOriCon,4x4x3
